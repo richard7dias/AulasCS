0ad620652d0a1832370120100071c3ba32aaceed
Richard Oliveira <richard.097@hotmail.com>
2022-10-09 14:41:30 -0300

    Commit1

--- Projeto1/HelloWorld/HelloWorld/Program.cs
+++ Projeto1/HelloWorld/HelloWorld/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
