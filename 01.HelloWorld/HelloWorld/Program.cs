5720240879be5ec80da7f69fd249188040cb5f4e
Richard Oliveira <richard.097@hotmail.com>
2022-10-09 15:34:42 -0300

    final CS

---
+++ 01.HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Tipos: Int, Float, Bool, Strting, Char, 
+            //String é com "" e Char é com ''
+            
+            string texto = "teste";
+            Console.WriteLine(texto);
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Meu nome é Richard");
+            Console.ReadLine();
+           
+
+            Console.WriteLine("Escreva seu nome: ");
+            String nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é:");
+            Console.WriteLine(nome);
+            
+        }
+    }
+}
---
+++ 02.Array/02.Array/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] jogos = new string[3] { "FIFA 22", "Rocket Legue", "Red Dead Redemption" };
+
+jogos[0] = "Fifa 23";
+
+Console.WriteLine(jogos[0]);
+Console.ReadLine();
